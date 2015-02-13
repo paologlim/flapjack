7142001b9d5b65d0b4d869affd407dd637f2f606
Paolo G. Lim <paolo@caresharing.ph>
2015-02-13 12:56:52 +0800

    666: Update sms_gammu
    refs: #666

--- lib/flapjack/gateways/sms_gammu.rb
+++ lib/flapjack/gateways/sms_gammu.rb
@@ -2,6 +2,14 @@
 
 require 'mysql2'
 
+require 'flapjack/redis_proxy'
+require 'flapjack/record_queue'
+require 'flapjack/utility'
+require 'flapjack/exceptions'
+
+require 'flapjack/data/alert'
+require 'flapjack/data/check'
+
 module Flapjack
   module Gateways
     class SmsGammu
@@ -10,78 +18,97 @@ module Flapjack
         VALUES ('%s', '%s', '%s', '%s', %s)
       SQL
 
-      class << self
+      attr_accessor :sent
 
-        include Flapjack::Utility
+      include Flapjack::Utility
 
-        def start
-          @sent = 0
+      def initialize(opts = {})
+        @lock   = opts[:lock]
+        @config = opts[:config]
+        @queue  = Flapjack::RecordQueue.new(@config['queue'] || 'sms_gammu_notifications',
+                                            Flapjack::Data::Alert)
+        @sent   = 0
+        @db     = Mysql2::Client.new(:host     => @config["mysql_host"],
+                                     :database => @config["mysql_database"],
+                                     :username => @config["mysql_username"],
+                                     :password => @config["mysql_password"])
 
-          if @config.nil? || (@config.respond_to?(:empty?) && @config.empty?)
-            @logger.error "sms_gammu config is missing"
-            return
+        Flapjack.logger.debug("new sms_gammu gateway pikelet with the following options: #{@config.inspect}")
+      end
+
+      def start
+        begin
+          Zermelo.redis = Flapjack.redis
+
+          loop do
+            @lock.synchronize do
+              @queue.foreach {|alert| handle_alert(alert) }
+            end
+
+            @queue.wait
           end
+        ensure
+          Flapjack.redis.quit
+        end
+      end
 
-          @db = Mysql2::Client.new(:host     => @config["mysql_host"],
-                                   :database => @config["mysql_database"],
-                                   :username => @config["mysql_username"],
-                                   :password => @config["mysql_password"])
+      def stop_type
+        :exception
+      end
+
+      private
+
+      def handle_alert(alert)
+        @alert  = alert
+        address = alert.medium.address
+        from    = @config["from"]
+        message = prepare_message
+        errors  = []
+
+        [[from,    "SMS from address is missing"],
+         [address, "SMS address is missing"]].each do |val_err|
+
+          next unless val_err.first.nil? || (val_err.first.respond_to?(:empty?) && val_err.first.empty?)
+          errors << val_err.last
         end
 
-        def prepare_message
-          message_type  = @alert.rollup ? 'rollup' : 'alert'
-          template_path = @config['templates']["#{message_type}.text"]
-          template      = ERB.new(File.read(template_path), nil, '-')
-
-          begin
-            message = template.result(binding).chomp
-            truncate(message, 159)
-          rescue => e
-            @logger.error "Error while excuting the ERB for an sms: " +
-              "ERB being executed: #{template_path}"
-            raise
-          end
+        unless errors.empty?
+          errors.each {|err| Flapjack.logger.error err }
+          return
         end
 
-        def send_message(message, from, to)
-          begin
-            @db.query(INSERT_QUERY % [Time.now, message, to, from, 1])
-            @sent += 1
-            @alert.record_send_success!
-            @logger.debug "Sent SMS via Gammu"
-          rescue => e
-            @logger.error "Failed to send SMS via Gammu: #{e.class}, #{e.message}"
-          end
-        end
+        send_message(message, from, address)
+      rescue => e
+        Flapjack.logger.error "Error generating or delivering sms to #{address}: #{e.class}: #{e.message}"
+        Flapjack.logger.error e.backtrace.join("\n")
+        raise
+      end
 
-        def perform(contents)
-          @logger.debug "Woo, got a notification to send out: #{contents.inspect}"
+      def prepare_message
+        message_type  = @alert.rollup ? 'rollup' : 'alert'
+        template_path = @config['templates']["#{message_type}.text"]
+        template      = ERB.new(File.read(template_path), nil, '-')
 
-          @alert  = Flapjack::Data::Alert.new(contents, :logger => @logger)
-          address = @alert.address
-          from    = @config["from"]
-          message = prepare_message
-          errors  = []
-
-          [[from,    "SMS from address is missing"],
-           [address, "SMS address is missing"]].each do |val_err|
-
-            next unless val_err.first.nil? || (val_err.first.respond_to?(:empty?) && val_err.first.empty?)
-            errors << val_err.last
-          end
-
-          unless errors.empty?
-            errors.each {|err| @logger.error err }
-            return
-          end
-
-          send_message(message, from, address)
+        begin
+          message = template.result(binding).chomp
+          truncate(message, 159)
         rescue => e
-          @logger.error "Error generating or delivering sms to #{contents['address']}: #{e.class}: #{e.message}"
-          @logger.error e.backtrace.join("\n")
+          Flapjack.logger.error "Error while excuting the ERB for an sms: " +
+            "ERB being executed: #{template_path}"
           raise
         end
       end
+
+      def send_message(message, from, to)
+        begin
+          @db.query(INSERT_QUERY % [Time.now, message, to, from, 1])
+          @sent += 1
+          Flapjack.logger.debug "Sent SMS via Gammu"
+        rescue => e
+          Flapjack.logger.error "Failed to send SMS via Gammu: #{e.class}, #{e.message}"
+        end
+      end
+
     end
   end
 end
